5d3360f17d771bb78f2bf5405468e31ed504375a
Josh Cepek <josh.cepek@usa.net>
2016-06-15 10:13:46 -0500

    Update queries with database ATTACHed prefix; minor cleanup
    Queries now use the expected DB attachment names: fcc or app. This
    allows queries to use either/both datasets without restarting a client
    with a new main DB opened.

--- queries/inactive-vanity.sql
+++ queries/inactive-vanity.sql
@@ -1,18 +1,18 @@
 CREATE TEMP TABLE tmp_ivc AS
 SELECT
-	sys_id,
-	callsign,
-	license_status,
-	grant_date,
-	max(
+	sys_id
+	,callsign
+	,license_status
+	,grant_date
+	,max(
 		(coalesce(
 			canceled_date,
 			expired_date
 		))
-	) AS end_date,
-	last_action_date
+	) AS end_date
+	,last_action_date
 FROM
-	t_hd
+	fcc.t_hd
 WHERE
 	length(callsign) = 4
 	AND
@@ -22,7 +22,7 @@ WHERE
 GROUP BY
 	callsign
 ORDER BY
-	end_date DESC,
-	callsign
+	end_date DESC
+	,callsign
 ;
 
