6c427a5f5d0d032e40980426ee6d8d8af3ba3a5b
Josh Cepek <josh.cepek@usa.net>
2016-06-17 10:30:46 -0500

    Offload SQL statement & data components to a new Data class
    Keep the import code cleaner and put all the data-specifics into its own
    class; an object of this class is called with a method indicating the
    query type and passed the table object as an argument.

--- import.pl
+++ import.pl
@@ -11,6 +11,7 @@ BEGIN {
 	my ($vol, $dir) = File::Spec->splitpath($0);
 	unshift @INC, File::Spec->catdir($dir, 'lib');
 };
+use JC::ULS::Data ();
 use JC::ULS::Table ();
 
 use constant {
@@ -255,157 +256,20 @@ sub mk_table {
 		die "mk_table(): missing arg $_" if (not exists $args{$_});
 	}
 
+	# SQL statement data encapsulation object:
+	my $data = JC::ULS::Data->new;
+
+	my $meth = $data->can("query_$args{name}")
+		or die "No support for table type: $args{name}";
+
 	my $table = JC::ULS::Table->new;
 	$table->define(
 		%args
 	) or die "Table $args{name} failed define: " . $table->error;
 
-	for ($args{name}) {
-		query_hd($table) if ($_ eq "HD");
-		query_am($table) if ($_ eq "AM");
-		query_en($table) if ($_ eq "EN");
-		query_ad($table) if ($_ eq "AD");
-		query_vc($table) if ($_ eq "VC");
-	}
+	# Set up the table object with processing statements:
+	$data->$meth( $table );
 
 	return $table;
-}
-
-sub query_hd {
-	my $table = shift;
-
-	$table->dateFields(
-		dates => [ 8..10, 43..44 ],
-	) or die "HD dates failed: " . $table->error;
-
-	$table->addQuery(
-		fields => [ 2..3, 5..10, 43..44 ],
-		sql => qq[
-			INSERT OR REPLACE INTO t_hd (
-				sys_id,
-				uls_fileno,
-				callsign,
-				license_status,
-				service_code,
-				grant_date,
-				expired_date,
-				canceled_date,
-				effective_date,
-				last_action_date
-			)
-			VALUES (?,?,?,?,?,?,?,?,?,?)
-		],
-	) or die "HD sth failed: " . $table->error;
-}
-
-sub query_am {
-	my $table = shift;
-
-	$table->addQuery(
-		fields => [ 2..3, 5..10, 13..14, 16..18 ],
-		sql => qq[
-			INSERT OR REPLACE INTO t_am (
-				sys_id,
-				uls_fileno,
-				callsign,
-				op_class,
-				group_code,
-				region_code,
-				trustee_callsign,
-				trustee_indicator,
-				sys_call_change,
-				vanity_call_change,
-				previous_callsign,
-				previous_op_class,
-				trustee_name
-			)
-			VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
-		],
-	) or die "AM sth failed: " . $table->error;
-}
-
-sub query_en {
-	my $table = shift;
-
-	$table->addQuery(
-		fields => [ 2..3, 5..12, 16..21, 23..24 ],
-		sql => qq[
-			INSERT OR REPLACE INTO t_en (
-				sys_id,
-				uls_fileno,
-				callsign,
-				entity_type,
-				license_id,
-				entity_name,
-				first_name,
-				mi,
-				last_name,
-				suffix,
-				street,
-				city,
-				state,
-				zip_code,
-				po_box,
-				attn,
-				frn,
-				type_code
-			)
-			VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
-		],
-	) or die "EN sth failed: " . $table->error;
-}
-
-sub query_ad {
-	my $table = shift;
-
-	$table->dateFields(
-		dates => [ 11, 22 ],
-	) or die "HD dates failed: " . $table->error;
-
-	$table->addQuery(
-		fields => [ 2..3, 5..6, 11, 16..17, 19, 22 ],
-		sql => qq[
-			INSERT OR REPLACE INTO t_ad (
-				sys_id,
-				uls_fileno,
-				purpose,
-				status,
-				receipt_date,
-				orig_purpose,
-				waver_req,
-				has_attachment,
-				entry_date
-			)
-			VALUES (?,?,?,?,?,?,?,?,?)
-		],
-	) or die "AD sth failed: " . $table->error;
-}
-
-sub query_vc {
-	my $table = shift;
-
-	if ( $table->get('update') ) {
-		$table->addQuery(
-			fields => [ 2 ],
-			sql => qq[
-				DELETE FROM t_vc
-				WHERE
-				sys_id = ?
-			],
-		) or die "VC sth (delete) failed: " . $table->error;
-	}
-
-	$table->addQuery(
-		fields=> [ 2..3, 5..6 ],
-		sql => qq[
-			INSERT OR REPLACE INTO t_vc (
-				sys_id,
-				uls_fileno,
-				pref_order,
-				callsign
-			)
-			VALUES (?,?,?,?)
-		],
-	) or die "VC sth failed: " . $table->error;
 }
 
---
+++ lib/JC/ULS/Data.pm
@@ -0,0 +1,165 @@
+package JC::ULS::Data;
+
+use strict;
+use warnings;
+#use Carp ();
+
+# --
+# Class-scoped vars
+# --
+
+# None.
+
+# --
+# Constructors
+# --
+
+sub new {
+	my $class = shift;
+	$class = ref($class) || $class;
+	bless { }, $class;
+}
+
+# --
+# Methods
+# --
+
+sub query_HD {
+	my ($self, $table) = (@_);
+
+	$table->dateFields(
+		dates => [ 8..10, 43..44 ],
+	) or die "HD dates failed: " . $table->error;
+
+	$table->addQuery(
+		fields => [ 2..3, 5..10, 43..44 ],
+		sql => qq[
+			INSERT OR REPLACE INTO t_hd (
+				sys_id,
+				uls_fileno,
+				callsign,
+				license_status,
+				service_code,
+				grant_date,
+				expired_date,
+				canceled_date,
+				effective_date,
+				last_action_date
+			)
+			VALUES (?,?,?,?,?,?,?,?,?,?)
+		],
+	) or die "HD sth failed: " . $table->error;
+}
+
+sub query_AM {
+	my ($self, $table) = (@_);
+
+	$table->addQuery(
+		fields => [ 2..3, 5..10, 13..14, 16..18 ],
+		sql => qq[
+			INSERT OR REPLACE INTO t_am (
+				sys_id,
+				uls_fileno,
+				callsign,
+				op_class,
+				group_code,
+				region_code,
+				trustee_callsign,
+				trustee_indicator,
+				sys_call_change,
+				vanity_call_change,
+				previous_callsign,
+				previous_op_class,
+				trustee_name
+			)
+			VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
+		],
+	) or die "AM sth failed: " . $table->error;
+}
+
+sub query_EN {
+	my ($self, $table) = (@_);
+
+	$table->addQuery(
+		fields => [ 2..3, 5..12, 16..21, 23..24 ],
+		sql => qq[
+			INSERT OR REPLACE INTO t_en (
+				sys_id,
+				uls_fileno,
+				callsign,
+				entity_type,
+				license_id,
+				entity_name,
+				first_name,
+				mi,
+				last_name,
+				suffix,
+				street,
+				city,
+				state,
+				zip_code,
+				po_box,
+				attn,
+				frn,
+				type_code
+			)
+			VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+		],
+	) or die "EN sth failed: " . $table->error;
+}
+
+sub query_AD {
+	my ($self, $table) = (@_);
+
+	$table->dateFields(
+		dates => [ 11, 22 ],
+	) or die "HD dates failed: " . $table->error;
+
+	$table->addQuery(
+		fields => [ 2..3, 5..6, 11, 16..17, 19, 22 ],
+		sql => qq[
+			INSERT OR REPLACE INTO t_ad (
+				sys_id,
+				uls_fileno,
+				purpose,
+				status,
+				receipt_date,
+				orig_purpose,
+				waver_req,
+				has_attachment,
+				entry_date
+			)
+			VALUES (?,?,?,?,?,?,?,?,?)
+		],
+	) or die "AD sth failed: " . $table->error;
+}
+
+sub query_VC {
+	my ($self, $table) = (@_);
+
+	if ( $table->get('update') ) {
+		$table->addQuery(
+			fields => [ 2 ],
+			sql => qq[
+				DELETE FROM t_vc
+				WHERE
+				sys_id = ?
+			],
+		) or die "VC sth (delete) failed: " . $table->error;
+	}
+
+	$table->addQuery(
+		fields=> [ 2..3, 5..6 ],
+		sql => qq[
+			INSERT OR REPLACE INTO t_vc (
+				sys_id,
+				uls_fileno,
+				pref_order,
+				callsign
+			)
+			VALUES (?,?,?,?)
+		],
+	) or die "VC sth failed: " . $table->error;
+}
+
+1;
